36344552e03fee19415968235a43f170a106478e
KojiIshikaw <koji.ishikawa@techshare.co.jp>
2024-05-20 07:38:13 +0900

    all installed successfully

--- install_unitree_ros2.sh
+++ install_unitree_ros2.sh
@@ -2,5 +2,15 @@
 cd unitree_ros2/cyclonedds_ws/src/
 git clone https://github.com/ros2/rmw_cyclonedds -b foxy
 git clone https://github.com/eclipse-cyclonedds/cyclonedds -b releases/0.10.x 
+#git clone https://github.com/ros2/rosidl.git
 cd ..
+sudo apt update
+sudo apt install ros-foxy-rmw-cyclonedds-cpp
+# sudo apt install ros-foxy-performance-test-fixture
 colcon build --packages-select cyclonedds
+source /opt/ros/foxy/setup.bash
+# colcon build --packages-select rmw_cyclonedds_cpp
+# colcon build --packages-select rosidl_pycommon
+# colcon build --packages-select unitree_api
+# colcon build --packages-select unitree_go
+colcon build
---
+++ modify_unitree_ros2_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 修正対象のファイル
+TARGET_FILE="unitree_ros2/setup.sh"
+
+# 変更箇所
+sed -i 's|source $HOME/unitree_ros2/cyclonedds_ws/install/setup.bash|source $HOME/Go2_demo/unitree_ros2/cyclonedds_ws/install/setup.bash|' $TARGET_FILE
+sed -i 's|<NetworkInterface name="enp3s0"|<NetworkInterface name="eth0"|' $TARGET_FILE
