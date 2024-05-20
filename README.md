d111ad8ec28d0af011f382073e5837c32501f1e3
KojiIshikaw <koji.ishikawa@techshare.co.jp>
2024-05-20 10:18:14 +0900

    fix

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 
-git clone https://github.com/TechShare-inc/go2_demo_kit --recursive 
+# FAQ
+https://techshare.co.jp/faq/unitree/go2_demo_secondary_development.html
+
+# download
+git clone https://github.com/TechShare-inc/go2_demo_kit --recursive Go2_demo
--- installer/install_unitree_ros2.sh
+++ installer/install_unitree_ros2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd unitree_ros2/cyclonedds_ws/src/
+cd ../unitree_ros2/cyclonedds_ws/src/
 git clone https://github.com/ros2/rmw_cyclonedds -b foxy
 git clone https://github.com/eclipse-cyclonedds/cyclonedds -b releases/0.10.x 
 #git clone https://github.com/ros2/rosidl.git
