200b09a1f5a2367369518562806e3ffb100483c8
KojiIshikaw <koji.ishikawa@techshare.co.jp>
2024-05-20 12:09:30 +0900

    fix modify_unitree_ros2_setup.sh

--- installer/modify_unitree_ros2_setup.sh
+++ installer/modify_unitree_ros2_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 修正対象のファイル
-TARGET_FILE="unitree_ros2/setup.sh"
+TARGET_FILE="../unitree_ros2/setup.sh"
 
 # 変更箇所
 sed -i 's|source $HOME/unitree_ros2/cyclonedds_ws/install/setup.bash|source $HOME/Go2_demo/unitree_ros2/cyclonedds_ws/install/setup.bash|' $TARGET_FILE
